935a2b517a96edbcf2d8b2013d715d81e1c8ea3e
Nora Granahan <namgranahan@gmail.com>
2021-11-11 14:21:49 -0500

    Create masthead component

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,16 @@
 <template>
   <!-- <Tutorial/> -->
   <main class="container">
-    <h1>homepage. helloooo.</h1>
+    <AppHeader />
   </main>
 </template>
 
 <script>
-export default {}
+import AppHeader from '@/components/AppHeader.vue';
+
+export default {
+  components: {
+    AppHeader
+  }
+}
 </script>
